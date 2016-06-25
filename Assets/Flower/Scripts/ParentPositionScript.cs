44380c6ee363a4366b9b69414a34e1647de40ed4
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-06-25 13:52:43 +0100

    Initial Project Commit
    Short flower animation

---
+++ Assets/Flower/Scripts/ParentPositionScript.cs
@@ -0,0 +1,20 @@
+﻿// Matthew Cormack
+// 24/06/16
+//
+// Parent the position of this gameobject to another's position
+// (i.e. without scaling, rotating)
+
+using UnityEngine;
+
+public class ParentPositionScript : MonoBehaviour
+{
+	public GameObject AttachObject = null;
+
+	void Update()
+	{
+		if ( AttachObject != null )
+		{
+			transform.position = AttachObject.transform.position;
+        }
+	}
+}
---
+++ Assets/Flower/Scripts/PulseTransformScript.cs
@@ -0,0 +1,42 @@
+﻿// Matthew Cormack
+// 24/06/16
+//
+// Pulsate the desired transform variables (in local space) with time
+// (i.e. position/rotation/scale)
+
+using UnityEngine;
+
+public class PulseTransformScript : MonoBehaviour
+{
+	// Public inspector
+	public Vector3 PulsePosition;
+	public Vector3 PulseRotation;
+	public Vector3 PulseScale;
+
+	public float PulseSpeed = 1;
+	public float PulseLimit = 1;
+	public float PulseOffset = 0;
+
+	// Private
+	private Vector3 CurrentPosition;
+	private Vector3 CurrentRotation;
+	private Vector3 CurrentScale;
+
+	void Update()
+	{
+		transform.localPosition -= CurrentPosition;
+		transform.localEulerAngles -= CurrentRotation;
+		transform.localScale -= CurrentScale;
+
+		// Pulsate
+		float change = Mathf.Sin( ( Time.time * PulseSpeed ) + PulseOffset ) * PulseLimit;
+		{
+			CurrentPosition = PulsePosition * change;
+			CurrentRotation = PulseRotation * change;
+			CurrentScale = PulseScale * change;
+		}
+		transform.localPosition += CurrentPosition;
+		transform.localEulerAngles += CurrentRotation;
+		transform.localScale += CurrentScale;
+	}
+}
